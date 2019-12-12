0f0ac242d1418c742e232568fc52d7944f148c6f
ekachner <ekibam@gmail.com>
2019-12-12 16:45:23 -0700

    Challenge Completed

--- answer.js
+++ answer.js
@@ -1,1 +1,35 @@
-// *** YOUR ANSWER BELOW ***
+"use strict";
+
+function drawWidth(w){
+    let x = "";
+    let y = w-1;
+    for(let i = 1; i < y; i++){
+        while(x.length < i){
+            x += "-";
+        }       
+    }
+    console.log( `+${x}+`);
+}
+
+
+function drawHeight(w,h){
+    let z = h-1;
+    for(let j = 1; j < z; j++){
+        let x = "";
+        let y = w-1;
+        for(let i = 1; i < y; i++){
+            while(x.length < i){
+                x += " ";
+            }
+        }
+        console.log(`|${x}|`);
+    }
+}
+
+
+function drawSquare(w,h){
+    drawWidth(w);
+    drawHeight(w,h);
+    drawWidth(w);
+}
+(drawSquare(15,9));
